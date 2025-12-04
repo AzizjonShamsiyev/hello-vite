237040214116eb518dece2adf704032c29300e9a
AzizjonShamsiyev <135442867+AzizjonShamsiyev@users.noreply.github.com>
2025-12-04 09:45:42 +0800

    Add v-for component - list rendering with index and item

---
+++ src/components/vfor.vue
@@ -0,0 +1,9 @@
+<template>
+    <div v-for="(item,index) in list" :key="index">
+        index is : {{ index }} --- element content is : {{ item }}
+    </div>
+</template>
+<script setup>
+    import { reactive, ref } from 'vue';
+    const list = reactive(['HTML', 'CSS', 'JavaScript', 'Vue']);
+</script>
