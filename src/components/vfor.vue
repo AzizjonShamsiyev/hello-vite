0fb7bd31f5c3b936d0b8f3f0d75a1164e113cac7
AzizjonShamsiyev <135442867+AzizjonShamsiyev@users.noreply.github.com>
2025-12-04 09:49:42 +0800

    Update v-for component - list rendering with reactive array of objects

--- src/components/vfor.vue
+++ src/components/vfor.vue
@@ -1,9 +1,18 @@
 <template>
-    <div v-for="(item,index) in list" :key="index">
+    <!-- <div v-for="(item,index) in list" :key="index">
         index is : {{ index }} --- element content is : {{ item }}
+    </div> -->
+    <div v-for="item in list" :key="item.id">
+        id is : {{ item.id }} --- element content is : {{ item.message }}
     </div>
 </template>
 <script setup>
     import { reactive, ref } from 'vue';
-    const list = reactive(['HTML', 'CSS', 'JavaScript', 'Vue']);
+    // const list = reactive(['HTML', 'CSS', 'JavaScript', 'Vue']);
+    const list = reactive([
+        { id: 1, message: 'HTML' },
+        { id: 2, message: 'CSS' },
+        { id: 3, message: 'JavaScript' },
+        { id: 4, message: 'Vue' }
+    ]);
 </script>
