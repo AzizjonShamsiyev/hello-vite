d23700b092a9184a9909c4820cf6c5e7e96537dd
AzizjonShamsiyev <135442867+AzizjonShamsiyev@users.noreply.github.com>
2025-12-04 09:34:43 +0800

    Add v-show component - compare v-if and v-show directives

---
+++ src/components/VShow.vue
@@ -0,0 +1,9 @@
+<template>
+    <p v-if="flag">v-if</p>
+    <p v-show="flag">v-show</p>
+    <button @click="flag = !flag">Click</button>
+</template>
+<script setup>
+    import { ref } from 'vue';
+    const flag = ref(true);
+</script>
