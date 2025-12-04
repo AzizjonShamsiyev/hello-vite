0163513e66bdf6b24cb9bf4c303205e7d94dc53e
AzizjonShamsiyev <135442867+AzizjonShamsiyev@users.noreply.github.com>
2025-12-04 10:34:33 +0800

    Add prevent component - demonstrate event bubbling and .stop modifier

---
+++ src/components/prevent.vue
@@ -0,0 +1,13 @@
+<template>
+  <div v-on:click="show('I am a father element even')">
+    <button v-on:click="show('I am a son element event')">
+      event bubbling
+    </button>
+    <button v-on:click.stop="show('I am a son element event')">
+      prevent event bubbling
+    </button>
+  </div>
+</template>
+<script setup>
+let show = (message) => console.log(message);
+</script>
